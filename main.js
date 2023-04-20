d91d00315b4e7f078b01acc5c07df308f444522c
mi <jsnnid@live.com>
2023-04-20 12:32:12 +0800

    '2023-04-20T04:32:10.788Z'

--- main.js
+++ main.js
@@ -78,9 +78,9 @@ if (cluster.isMaster) {
             exec("git add .", function (error, stdout, stderr) {
                 if (error) {
                     console.log(error);
-                    return;
+                }else{
+                    console.log("add", stdout)
                 }
-                console.log("add", stdout)
 
 
                 exec("git commit -m '" + new Date().toISOString() + "'", async function (error, stdout, stderr) {
